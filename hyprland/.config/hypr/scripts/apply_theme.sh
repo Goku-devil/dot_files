d86371d5c365149f7b49b53e43bb27b0202f7099
Goku-devil <gokugoku0156@gmail.com>
2026-06-09 18:36:37 +0530

    Theme switcher for nvim

--- hyprland/.config/hypr/scripts/apply_theme.sh
+++ hyprland/.config/hypr/scripts/apply_theme.sh
@@ -7,6 +7,8 @@ fi
 
 # 1. Write the state so Lua and Bash know what to load
 echo "$theme" > ~/.config/hypr/.theme_state
+# Update Neovim theme state
+echo "$1" > ~/.config/nvim/theme.txt
 
 # 2. Swap Symlinks
 ln -sf ~/.config/waybar/themes/${theme}.css ~/.config/waybar/style.css
