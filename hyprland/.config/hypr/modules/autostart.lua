400058ea80f535148831ffa36835895e80370214
Goku-devil <gokugoku0156@gmail.com>
2026-06-11 22:15:01 +0530

    bug fix

--- hyprland/.config/hypr/modules/autostart.lua
+++ hyprland/.config/hypr/modules/autostart.lua
@@ -2,8 +2,22 @@
 ---- AUTOSTART ----
 -------------------
 
+local state_file = os.getenv("HOME") .. "/.config/hypr/.theme_state"
+local function get_current_theme()
+    local f = io.open(state_file, "r")
+    if f then
+        local theme = f:read("*l")
+        f:close()
+        if theme and theme ~= "" then
+            return theme
+        end
+    end
+    return "nord"
+end
+
 
 hl.on("hyprland.start", function () 
+    os.execute("~/.config/hypr/scripts/apply_theme.sh " .. get_current_theme())
     hl.exec_cmd("waybar")
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("swaync")
