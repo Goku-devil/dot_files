d2ccf2a39a05827442c7ea355f8b77da81cd384d
Goku-devil <gokugoku0156@gmail.com>
2026-07-03 23:41:35 +0530

    Changed the default screenshot directory

--- hyprland/.config/hypr/modules/env.lua
+++ hyprland/.config/hypr/modules/env.lua
@@ -1,7 +1,10 @@
-
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+-- Set global save directory for Hyprshot
+hl.env("HYPRSHOT_DIR", os.getenv("HOME") .. "/Pictures/Screenshots")
+
